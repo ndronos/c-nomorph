9187b217b9d2d516534eca1ff96107ee5ee5bc0b
Leandro Leite <ndronos@gmail.com>
2017-02-06 12:58:09 -0200

    (Ingneus,2015) Commented build

---
+++ 00-hello/hello-commented.c
@@ -0,0 +1,33 @@
+// (Igneus, 2015) C on Mac
+
+//
+// main.c
+// hello-xcode
+//
+// Created by ... on ...
+// Copyright ... All rights reserved.         //  Comment section, not used to convert in an application
+
+#include <stdio.h>                            //  preprocessor command,
+                                              //  include file on agular brackets `< >`
+                                              //  `.h` : header file, high importance
+
+/*
+int main(int argc, const char * argv[]) {
+*/
+
+
+int main() {                                  //  At least one function in C program, compulsory
+                                              //  `main` function
+                                              //  `int` : integer, related to `return 0`;
+    // insert code here...
+    
+    printf("Hello, Notes!\n");                //  `printf()` function contained in `<stdio.h>`
+                                              //  `\n`: new line
+                                              //  `;` :`semicollon, indicate end of the line
+
+    return 0;                                 //  terminate main() function 
+                                              //  `return` `int` value 0, executed perfectly
+                                              //  Message: `Program ended with exit code: 0`
+
+}                                             //  `{ }`: brackets, definition of the function 
+
