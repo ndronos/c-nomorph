471c1144a7dd711cedf66fe239083dc975460f1c
Leandro Leite <ndronos@gmail.com>
2017-02-08 08:40:58 -0200

    (Igneus, 2015) Datatypes notes

---
+++ 01-datatypes/datatypes.c
@@ -0,0 +1,15 @@
+// (Igneus, 2015) Data Types in C: manage to NOT show any datatype in the entire lesson!
+
+/*
+  Basic           integer and floating point values
+
+  Enumerated      kind arithmetic type
+                  only assign to certain discrete integer values
+
+  void            keep space blank
+                  if unknown value is about to come
+                * void and zero are different things *
+
+  derived         collection of data
+
+*/
