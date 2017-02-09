0b416f14050acc493e79479c7b44a41aa027390a
Leandro Leite <ndronos@gmail.com>
2017-02-09 06:30:22 -0200

    (Igneus, 2015) Format Specifiers code tryouts

--- 04-format-specifiers.c
+++ 04-format-specifiers.c
@@ -25,6 +25,18 @@ $ clang 04-format-specifiers.c
 1 warning generated.
 */
 
+
+    int     value   = 10;
+    double  Double  = 10.5;
+    char    Char    = 'c';
+
+    printf(" %d\n %d\n %d\n", value, Double, Char);
+//  ... 33:39: warning: format specifies type 'int' but the argument has type 'double' [-Wformat]
+//  printf(" %d\n %d\n %d\n ", value, Double, Char);
+//                ~~                  ^~~~~~
+//                %f
+
+
     return 0;
 
 }
