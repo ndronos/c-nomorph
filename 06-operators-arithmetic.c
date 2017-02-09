d5f2b3a8fe34284b83e386098fe99edf2dbc9b2f
Leandro Leite <ndronos@gmail.com>
2017-02-09 11:20:20 -0200

    (Igneus, 2015) Arithmetic Operations code tryouts

---
+++ 06-operators-arithmetic.c
@@ -0,0 +1,20 @@
+// (Igneus, 2015) Artithmetic Operations
+
+#include <stdio.h>
+
+int main() {
+
+    int life = 10;
+    int bonus = 2;
+    int factor;
+
+    factor = life + bonus;
+
+    printf("Value of factor is: %d \n", factor);
+
+
+
+    return 0;
+
+}
+
