ae0c050ca317dc34abaa0cf864af895b9d14c4de
Leandro Leite <ndronos@gmail.com>
2017-02-08 23:50:00 -0200

    (Igneus, 2015) Constant notes

---
+++ 03-constants/constants.c
@@ -0,0 +1,13 @@
+// (Igneus, 2015) Constants
+
+/*
+  Constants     declare as much as possible
+                reducing dependencies
+
+  Two ways      using `#define`, simpler to do, in CAPS and no `=` sign
+  to declare    using `const` keyword
+
+*/
+
+
+
