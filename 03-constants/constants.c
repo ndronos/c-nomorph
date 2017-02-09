02114069a2bf9b281513a1ac7336c2efc781eb53
Leandro Leite <ndronos@gmail.com>
2017-02-08 23:59:01 -0200

    (Igneus, 2015) Constant code tryouts

--- 03-constants/constants.c
+++ 03-constants/constants.c
@@ -5,14 +5,16 @@
                 reducing dependencies
 
   Two ways      using `#define`, simpler to do, in CAPS and no `=` sign
-  to declare    using `const` keyword
+  to declare    using `const` keyword, easier
 
 */
 
 
 #include <stdio.h>
 
-
+/*
+#define
+*/
 #define LIFE 5
 #define BLOOD 3
 
@@ -23,6 +25,17 @@ int main() {
 
     printf("%d \n", value);
 
+/*
+    const
+*/
+    const int LIF3 = 10;
+    const int BL00D = 3;
+
+    int v4lue = LIF3 * BL00D;    
+
+    printf("%d \n", v4lue);
+
     return 0;
+
 }
 
