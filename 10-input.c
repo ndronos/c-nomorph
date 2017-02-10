095461d9cc3a12d852f938e629941b7564927cd4
Leandro Leite <ndronos@gmail.com>
2017-02-10 09:35:59 -0200

    (Igneus, 2015) Input code tryouts

---
+++ 10-input.c
@@ -0,0 +1,24 @@
+// (Igneus, 2005) Input, Read Number
+
+#include <stdio.h>
+
+int main() {
+
+    int value1, value2, sum;
+
+    printf("\n Enter the value #1: ");
+
+    printf("\n Enter the value #2: ");
+
+
+    sum = value1 + value2;
+
+    printf("\n The sum is: %d \n\n", sum);
+
+
+
+
+    return 0;    
+
+}
+
