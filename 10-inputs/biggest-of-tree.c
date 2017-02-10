173a994afcb524c2779395fe76ed6744f7447cd0
Leandro Leite <ndronos@gmail.com>
2017-02-10 10:07:49 -0200

    (Igneus, 2015) Input app, code tryouts

--- 10-inputs/biggest-of-tree.c
+++ 10-inputs/biggest-of-tree.c
@@ -8,7 +8,7 @@
 int main() {
 
     int value1, value2, value3, big = 0;   // initialized as zero
-/*
+
     printf("\n Enter the value #1: ");
     scanf("%d", &value1);
 
@@ -17,6 +17,11 @@ int main() {
     scanf("%d", &value2);
 
 
+    printf("\n Enter the value #3: ");
+    scanf("%d", &value3);
+
+
+/*
     sum = value1 + value2;
 
     printf("\n The sum is: %d \n\n", sum);
