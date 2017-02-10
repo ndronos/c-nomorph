6363f995882d1a50cd8489c8281fe2c02338514d
Leandro Leite <ndronos@gmail.com>
2017-02-10 08:20:46 -0200

    (Igneus, 2015) Decisions code tryouts

---
+++ 08-decisons-if-else.c
@@ -0,0 +1,23 @@
+// (Igneus, 2015) Decision IF-ELSE
+
+/*
+    if (condition) {
+        statements
+    }
+ 
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    int highScore = 100;
+
+
+
+
+    return 0;
+
+}
+
