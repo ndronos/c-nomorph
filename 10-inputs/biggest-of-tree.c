6520ae64d34f919aaa1ca39e46b7652b8eb9defc
Leandro Leite <ndronos@gmail.com>
2017-02-10 10:05:17 -0200

    (Igneus, 2015) Input app, code tryouts

---
+++ 10-inputs/biggest-of-tree.c
@@ -0,0 +1,30 @@
+// (Igneus, 2005) Input, Biigest of Three
+
+//  Good practice         Initialze values                 
+//                        some may contain `garbage values`, unexpected values
+
+#include <stdio.h>
+
+int main() {
+
+    int value1, value2, value3, big = 0;   // initialized as zero
+/*
+    printf("\n Enter the value #1: ");
+    scanf("%d", &value1);
+
+
+    printf("\n Enter the value #2: ");
+    scanf("%d", &value2);
+
+
+    sum = value1 + value2;
+
+    printf("\n The sum is: %d \n\n", sum);
+*/
+
+    getchar();    //on Windows, keep CLI window open
+
+    return 0;    
+
+}
+
