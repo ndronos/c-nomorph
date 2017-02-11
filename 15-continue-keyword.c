be9d15ccc2904ec80c1f2e140f77907ede4cb130
Leandro Leite <ndronos@gmail.com>
2017-02-11 07:12:39 -0200

    (Igneus, 2015) Continue Keyword code tryouts

--- 15-continue-keyword.c
+++ 15-continue-keyword.c
@@ -10,12 +10,17 @@ int main() {
     int value = 5;
 
     do {
-    
+ 
+    //  if (value == 11) continue;                // condition check  // but get infinite loop
+        if (value == 11) { value++; continue; }   // condition check  // get right value
+
         printf("%d  ", value);
 
         value++;
 
     } while (value <= 55);
+
+
 
 
     return 0;
