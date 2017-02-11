db000ca8f3a3db1d320111f5a40749d9dd00dcb8
Leandro Leite <ndronos@gmail.com>
2017-02-10 23:28:06 -0200

    (Igneus, 2015) Do While Loop notes

---
+++ 12-do-while-loop.c
@@ -0,0 +1,16 @@
+// (Ingeus, 2015) Do While Loop
+
+/*
+    do {                      Execute at least once
+      statements
+    } while (condition)       then test the condition
+*/
+
+#include <stdio.h>
+
+int main() {
+
+
+    return 0;
+}
+
