f57fb0671eb5d8fc60ac824b9b18153889705785
Leandro Leite <ndronos@gmail.com>
2017-02-11 06:51:29 -0200

    (Igneus, 2015) Break Keyword code tryouts

---
+++ 14-break-keyword.c
@@ -0,0 +1,22 @@
+// (Igneus, 2015) Break Keyword
+
+#include <stdio.h>
+
+int main() {
+
+    int value = 5;
+
+    while (value <= 55) {
+
+        printf("%d  ", value);
+
+        value++;
+
+    }
+
+
+
+    return 0;
+
+}
+
