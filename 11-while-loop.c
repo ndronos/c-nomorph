b13d24a2b399ab46df39d0d988654f6863b6800a
Leandro Leite <ndronos@gmail.com>
2017-02-10 23:21:29 -0200

    (Igneus, 2015) While Loop

---
+++ 11-while-loop.c
@@ -0,0 +1,24 @@
+// (Ingeus, 2015) While Loop
+
+#include <stdio.h>
+
+int main() {
+
+    int number = 5;
+
+    while (number <= 55)  {
+
+        printf("%d  ", number);
+
+        number++;                     // number = number + 1
+
+    }
+
+    printf("\n");
+
+
+
+    return 0;
+  
+}
+
