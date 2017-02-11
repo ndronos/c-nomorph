c0068eb23178f48b55b593077f89f1ed462536ea
Leandro Leite <ndronos@gmail.com>
2017-02-11 18:21:27 -0200

    (Igneus, 2015) Custom Functions notes

--- 20-functions-custom.c
+++ 20-functions-custom.c
@@ -1,14 +1,19 @@
 // (Igneus, 2015) Custom Functions
 
 //  At least one function
-//  the main function
+//  the main function         keep at bottom
 
 #include <stdio.h>
 
-// latest standard
-int main() {
-    return 0;
-}
+
+
+
+
+
+//  latest standard
+//  int main() {
+//      return 0;
+//  }
 
 
 //  old days...      // Program ended with exit code: 14
