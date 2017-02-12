375b4a5ea5ea178196288bb0697078725dc3b464
Leandro Leite <ndronos@gmail.com>
2017-02-12 10:01:56 -0200

    (Igneus, 2015) Apps code tryouts

---
+++ 20-apps/sum-of-tree-digits.c
@@ -0,0 +1,16 @@
+// (Igneus, 2015) Sum of Three Digits
+
+#include <stdio.h>
+
+int main() {
+
+    int number, digit1, digi2, digi3, sum  = 0;
+
+    printf("Enter a three digit number: ");
+    scanf("%d", &number);
+
+
+
+    return 0;
+}
+
