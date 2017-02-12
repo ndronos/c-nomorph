e36055a0be32c7ff5b02791236c096d6eb5e2309
Leandro Leite <ndronos@gmail.com>
2017-02-12 10:18:20 -0200

    (Igneus, 2015) Apps code tryouts

---
+++ 20-apps/armstrong-number.c
@@ -0,0 +1,12 @@
+// (Igneus, 2015) Armstrong Number
+
+// a number such that the sum of its digits raised to the third power is equal to the number itself.
+// 3**3 + 7**3 + 1**3 = 371
+
+#include <stdio.h>
+
+int main() {
+
+
+}
+
