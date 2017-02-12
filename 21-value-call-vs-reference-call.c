5c56010d6a8e01e16371c628d0c75e041ad847a8
Leandro Leite <ndronos@gmail.com>
2017-02-11 23:13:32 -0200

    (Igneus, 2015) Value/Reference Call code tryouts

--- 21-value-call-vs-reference-call.c
+++ 21-value-call-vs-reference-call.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 
 
+int swap(int v1, int v2) {                    // new function
+
+    return 0;
+}
+
+
 int main() {
 
     int value1 = 5;
@@ -10,6 +16,13 @@ int main() {
 
     printf("Value 1 is %d \b\n", value1);
     printf("Value 2 is %d \b\n", value2);
+
+
+    swap(value1, value2);                     // new function
+
+    printf("Swap value 1: %d \n", value1);
+    printf("Swap value 2: %d \n", value2);
+
 
 
     return 0;
