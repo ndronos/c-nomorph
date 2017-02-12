ae095c508d09b7855389a348c3e8859097d0b832
Leandro Leite <ndronos@gmail.com>
2017-02-11 22:17:01 -0200

    (Igneus, 2015) Custom Functions code tryouts

--- 20-functions-custom.c
+++ 20-functions-custom.c
@@ -2,8 +2,15 @@
 
 //  At least one function
 //  the main function         keep at bottom
+//  all funtions              on the top
+//  alternative               function on the bottom, 
+
 
 #include <stdio.h>
+
+
+int bottomfunction(int parameter1);         // function declared at bottom
+
 
 int cal(int value1, int value2) {
 
@@ -25,9 +32,17 @@ int main() {
     highest = cal(highScore1, highScore2);
     printf("The high score is: %d \n", highest);
 
+    printf("%d \n", bottomfunction(highest));
+
     return 0;
 }
 
+
+int bottomfunction(int argument1) {
+
+    return argument1;
+
+}
 
 
 //  latest standard
