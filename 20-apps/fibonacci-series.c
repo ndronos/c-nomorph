dcadc87845e3590bf99aa320f4c623b5e4a18336
Leandro Leite <ndronos@gmail.com>
2017-02-12 14:05:34 -0200

    (Igneus, 2015) Apps code tryouts

--- 20-apps/fibonacci-series.c
+++ 20-apps/fibonacci-series.c
@@ -1,8 +1,12 @@
 // (Igneus, 2015) Fibonacci Series
 
+//  series of numbers... the next number is found by adding up the two numbers before it.
+//  0 1 1 2 3 5 8 13 21 34...
+
 #include <stdio.h>
 
 int main() {
+
 
 
 
