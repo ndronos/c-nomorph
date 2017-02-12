863d8e6e5ffce32305703e631f0b45e6e93794b0
Leandro Leite <ndronos@gmail.com>
2017-02-12 10:09:06 -0200

    (Igneus, 2015) Apps code tryouts

--- 20-apps/sum-of-tree-digits.c
+++ 20-apps/sum-of-tree-digits.c
@@ -10,9 +10,13 @@ int main() {
     scanf("%d", &number);
 
     digit3 = number / 100;
+    digit2 = (number % 100) / 10;
+    digit1 = number % 10;
 
 
     printf("%d \n", digit3);
+    printf("%d \n", digit2);
+    printf("%d \n", digit1);
 
 
 
