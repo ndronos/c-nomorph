184ffb357d4774295d6ff74a2eba2a7993cdbeb8
Leandro Leite <ndronos@gmail.com>
2017-02-12 12:42:59 -0200

    (Igneus, 2015) Apps code tryouts

--- 20-apps/odds-in-hundred.c
+++ 20-apps/odds-in-hundred.c
@@ -4,14 +4,21 @@
 
 int main() {
 
-    int oddy;
 
-    for(oddy = 1; oddy <= 100 ; oddy += 2) {
+    for(int oddy = 1; oddy <= 100; oddy += 2) {
 
         printf("%d  ", oddy);
 
     }
 
+
+    printf("\n");
+
+    // even
+    for(int even = 0; even <= 100; even += 2) {
+
+        printf("%d  ", even);
+    }
 
 
     return 0;
