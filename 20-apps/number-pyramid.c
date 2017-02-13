f3fff7ad1693fab10bbc93cf40fd07fa65cea35a
Leandro Leite <ndronos@gmail.com>
2017-02-13 07:02:37 -0200

    (Igneus, 2015) Apps code tryouts

---
+++ 20-apps/number-pyramid.c
@@ -0,0 +1,19 @@
+// (Igneus, 2015) Number Pyramid
+
+#include <stdio.h>
+
+int main() {
+
+for(int row=1; row <=9; row++) {
+
+    for(int py=1; py <= row; py++) {
+      
+        printf("%d", py);
+    }
+
+    printf("\n");
+
+}
+    return 0;
+}
+
