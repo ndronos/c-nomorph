0f4b8f0e4cc59e4799d412ddce67ee9fe125f518
Leandro Leite <ndronos@gmail.com>
2017-02-16 11:07:02 -0200

    (Klemens, 2015) Add C 101 code

--- C-101/ten_pi.c
+++ C-101/ten_pi.c
@@ -1,4 +1,5 @@
-//
+// (Klemens, 2015) Appendix A: C 101 pp. 347
+// Variables have to be declared
 
 #include <stdio.h>
 #include <math.h>                                             // M_PI
@@ -13,12 +14,13 @@ int main(){
 
     printf("FAILING: count2 uninitialized... \n");
     printf("%g times %i = %g.\n", pi, count2, pi*count2);     // count2 value is 0  // warnining: ... [-Wuninitialized]
-    printf("%g times %i = %g.\n", pi, count2=20, pi*count2);  // count2 value is 20, but pi*count2 value is 0!
+    printf("%g times %i = %g.\n", pi, count2=20, pi*count2);  // warning: ... may be undefined [-Wsequence-point] 
+                                                              // count2 value is 20, but pi*count2 value is 0!
     printf("count2 value is %i \n", count2);                  // check: count2 value is 20!
     printf("%g times %i = %g.\n", pi, count2, pi*count2);     // expected: initialized as 20! 
 
     printf("M_PI = %g \n", M_PI);                             // M_PI
-    printf("%g times %i = %g.\n", M_PI, count3, M_PI*count3);   // M_PI
+    printf("%g times %i = %g.\n", M_PI, count3, M_PI*count3); // M_PI
 
 }
 
