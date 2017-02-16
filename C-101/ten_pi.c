c5782d96eed7ec3dcb4455bee3f864abf8141d58
Leandro Leite <ndronos@gmail.com>
2017-02-16 12:56:13 -0200

    (Klemens, 2015) Add C 101 code

--- C-101/ten_pi.c
+++ C-101/ten_pi.c
@@ -7,7 +7,7 @@
 int main(){
 
     double pi= 3.14159265;                              //POSIX defines the constant M_PI in math.h, by the way. // M_PI
-    int count= 10, count2, count3=30;                   //count2 is unitialized
+    int count= 10, count2, count3=30;                   //count2 is uninitialized
 
     printf("%g \n", pi);
     printf("%g times %i = %g.\n", pi, count, pi*count);
@@ -15,7 +15,9 @@ int main(){
     printf("FAILING: count2 uninitialized... \n");
     printf("%g times %i = %g.\n", pi, count2, pi*count2);     // count2 value is 0  // warnining: ... [-Wuninitialized]
     printf("%g times %i = %g.\n", pi, count2=20, pi*count2);  // warning: ... may be undefined [-Wsequence-point] 
-                                                              // count2 value is 20, but pi*count2 value is 0!
+                                                              // count2 value is 20, but pi*count2 for GCC value is 0,
+                                                              // both lines, for CLang after assignment value is correct
+                                                              // but uninitialized count2 has changeable (random?) value.
     printf("count2 value is %i \n", count2);                  // check: count2 value is 20!
     printf("%g times %i = %g.\n", pi, count2, pi*count2);     // expected: initialized as 20! 
 
