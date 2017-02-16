5fdf1ac27ddeea85dec69a4b1a79bb2447ef48ea
Leandro Leite <ndronos@gmail.com>
2017-02-16 10:12:34 -0200

    (Klemens, 2015) Add C 101 code

--- C-101/ten_pi.c
+++ C-101/ten_pi.c
@@ -5,7 +5,7 @@
 int main(){
 
     double pi= 3.14159265;                              //POSIX defines the constant M_PI in math.h, by the way.
-    int count= 10, count2, count3=30;                   //count2 is unitialized
+    int count= 10, count2, count3=30;                   //count2 is unitialized // warning: ... [-Wunitialized]
 
     printf("%g times %i = %g.\n", pi, count, pi*count);
 
