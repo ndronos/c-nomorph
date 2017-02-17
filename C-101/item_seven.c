075945933bbf18ff9744233a9d2b2a6e167bf75f
Leandro Leite <ndronos@gmail.com>
2017-02-17 10:26:22 -0200

    (Klemens, 2015) Add C 101 code

---
+++ C-101/item_seven.c
@@ -0,0 +1,17 @@
+// (Klemens, 2015) Appendix A: C 101 pp. 348-349
+
+#include <stdio.h>
+
+int intlist[10];
+
+int main(){
+
+    int len=20;
+    char string[len];
+
+    intlist[7] = 7;
+    snprintf(string, len, "Item seven is %i.", intlist[7]);
+    printf("string says: <<%s>>\n", string);
+
+}
+
