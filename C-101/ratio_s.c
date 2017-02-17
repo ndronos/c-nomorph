bcdf74ec8878579a316e1a6d4852a12e59e62ba3
Leandro Leite <ndronos@gmail.com>
2017-02-17 11:15:49 -0200

    (Klemens, 2015) Add C 101 code

--- C-101/ratio_s.c
+++ C-101/ratio_s.c
@@ -2,6 +2,15 @@
 
 #include <stdio.h>
 
+typedef struct {
+} ratio_s;
+
+ratio_s new_ratio(int num, int den);
+
+void print_ratio(ratio_s r);
+
+ratio_s ratio_add(ratio_s left, ratio_s right);
+
 int main() {
     ratio_s twothirds = new_ratio(2, 3);
     ratio_s aquarter = new_ratio(1, 4);
