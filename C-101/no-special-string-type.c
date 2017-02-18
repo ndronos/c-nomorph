07034bc1b87c300d0a4c04d6c2c3e397edb05217
Leandro Leite <ndronos@gmail.com>
2017-02-18 00:54:04 -0200

    (Klemens, 2015) Add C 101 code

--- C-101/no-special-string-type.c
+++ C-101/no-special-string-type.c
@@ -26,12 +26,6 @@ int main(){
     snprintf(str2, 100, "str1 says: %s", str1);
     printf("%s \n", str2);
   
-/*
-    printf("size of two ints: %zu\n", 2*sizeof(int));
-    printf("size of two ints: %zu\n", sizeof(int[2]));
-    printf("size of a double: %zu\n", sizeof(double));
-    printf("size of a ratio_s struct: %zu\n", sizeof(ratio_s));
-*/
 
 }
 
