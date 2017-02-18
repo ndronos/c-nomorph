a996a08bac5da0478bd576ea403363a3aadac5f0
Leandro Leite <ndronos@gmail.com>
2017-02-18 00:25:13 -0200

    (Klemens, 2015) Add C 101 code

--- C-101/no-special-string-type.c
+++ C-101/no-special-string-type.c
@@ -4,17 +4,24 @@
 
 #include <string.h>
 
-
 int main(){
 
     char* str = "Hello";
     printf("%s \n", str);   
 
-    str[4]='\0'; // warning: format specifies type 'double' but the argument has type 'char *' [-Wformat]
-    printf("%s \n", str); // Segmentation fault (core dumped)
+//  str[4]='\0'; // warning: format specifies type 'double' but the argument has type 'char *' [-Wformat]
+//  printf("%s \n", str); // Segmentation fault (core dumped)
 //  printf("%f \n", str); // warning: format ‘%f’ expects argument of type ‘double’, but argument 2 has type ‘char *’ [-Wformat=]
  
- 
+//  string.h
+    char *str1 = "hello", str2[100];
+    strlen(str1);
+//  printf("%lu \n", strlen(str1));   // long unsigned int   
+    strncpy(str2, 100, str1);
+    strncat(str2, 100, str1);
+    strncmp(str1, str2);
+    snprintf(str2, 100, "str1 says: %s", str1);
+
   
 /*
     printf("size of two ints: %zu\n", 2*sizeof(int));
