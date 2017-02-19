a9b21933e13b2c75dd21fa99e46e67b835bb064b
Leandro Leite <ndronos@gmail.com>
2017-02-19 11:37:05 -0300

    (Klemens, 2015) Add C 101 code

--- is-Punk-Rock/an-expansive-definition-of-truth.c
+++ is-Punk-Rock/an-expansive-definition-of-truth.c
@@ -12,20 +12,20 @@ int main(){
 
     int y=900;                                  printf("\n");
 
-    printf("%d \t\t\t\t",   y);                 printf("     y              \n");
+    printf("%d \t\t\t\t",   y);                 printf("       y         \n");
 
-    printf("%d \t\t\t\t",   x==y);              printf("  x==y              \n");
-    printf("%d \t\t\t\t",   x!=y);              printf("  x!=y              \n");
-    printf("%d \t\t\t\t",   x<y);               printf("  x<y               \n");
-    printf("%d \t\t\t\t",   x<=y);              printf("  x<=y              \n");
-    printf("%d \t\t\t\t",   x||y);              printf("  x||y              \n");
-    printf("%d \t\t\t\t",   x&&y);              printf("  x&&y              \n");
+    printf("%d \t\t\t\t",   x == y);            printf("  x == y         \n");
+    printf("%d \t\t\t\t",   x != y);            printf("  x != y         \n");
+    printf("%d \t\t\t\t",   x <  y);            printf("  x <  y         \n");
+    printf("%d \t\t\t\t",   x <= y);            printf("  x <= y         \n");
+    printf("%d \t\t\t\t",   x || y);            printf("  x || y         \n");
+    printf("%d \t\t\t\t",   x && y);            printf("  x && y         \n");
 
     int z=2000;                                 printf("\n");
 
     printf("%d \t\t\t\t",   z);                 printf("                z   \n");
 
-    printf("%d \t\t\t\t",   y > y || y >= z );  printf("  x > y || y >= z   \n");
+    printf("%d \t\t\t\t",   x > y || y >= z );  printf("  x > y || y >= z   \n");
 
                                                 printf("\n");
 
