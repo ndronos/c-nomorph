5451b29cf8632f85b0be0723bba3be7a277698b2
Leandro Leite <ndronos@gmail.com>
2017-02-19 13:22:13 -0300

    (Klemens, 2015) Add C 101 code

---
+++ is-Punk-Rock/divisions.c
@@ -0,0 +1,12 @@
+// (Klemens, 2015) Appendix A: C 101 pp. 355
+
+#include <stdio.h>
+
+int main(){
+
+    printf("13./5=%g \n", 13./5);
+    printf("13/5=%i  \n", 13/5);
+    printf("13%%5=%i \n", 13%5);
+
+}
+
