d166dbd6cb7ecc3eafeab8b9553810cf5122dc8b
Leandro Leite <ndronos@gmail.com>
2017-02-19 10:59:35 -0300

    (Klemens, 2015) Add C 101 code

---
+++ is-Punk-Rock/an-expansive-definition-of-truth.c
@@ -0,0 +1,36 @@
+// (Klemens, 2015) Appendix A: C 101 pp. 354-354
+
+#include <stdio.h>
+
+int main(){
+
+    int x=100;                                  printf("\n");
+
+    printf("%d \t\t\t\t",   x);                 printf("  x              \n");
+    printf("%d \t\t\t\t",  !x);                 printf(" !x              \n");
+    printf("%d \t\t\t\t", !!x);                 printf("!!x              \n");
+
+
+
+                                                printf("\n");
+
+/*
+
+    !x                  //not x
+    x==y                //x equals y
+    x != y              //x is not equal to y
+    x < y               //x is less than y
+    x <= y              //x is less than equal to y
+    x || y              //x or y
+    x && y              //x and y
+    x > y || y >= z     //x is greather than y or is greater than or equal to z
+
+
+For example, if `x` is any nonzero value, then `!x` evaluates to zero,
+and `!!x` evaluates to one.
+
+*/
+
+
+
+}
