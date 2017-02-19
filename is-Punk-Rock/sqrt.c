32531fe98f24e289d19feecb53d7ba11c1f5d9f0
Leandro Leite <ndronos@gmail.com>
2017-02-19 13:54:33 -0300

    (Klemens, 2015) Add C 101 code

--- is-Punk-Rock/sqrt.c
+++ is-Punk-Rock/sqrt.c
@@ -5,10 +5,16 @@
 
 int main(){
 
-    double x;         // = 49;
-    scanf("%lf", &x);
+    int x;            //double x;         // = 49;
+    printf("Enter #: ");
+    scanf("%i", &x);  //scanf("%lf", &x);
 
-    printf("The truncated square root of %f is %g. \n", x, x > 0 ? sqrt(x) : 0);
+    printf("The truncated square root of %i is %g. \n", x, x > 0 ? sqrt(x) : 0);
+
+//  double / %lf / %f
+//          The truncated square root of 9.000000 is 3.
+//  printf("The truncated square root of %f is %g. \n", x, x > 0 ? sqrt(x) : 0);
+
 //  printf("The truncated square root of  x is %g. \n",    x > 0 ? sqrt(x) : 0);
 
 }
