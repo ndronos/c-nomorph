2038ed8b9179e33768746a17b9121552df67161b
Leandro Leite <ndronos@gmail.com>
2017-02-19 11:28:33 -0300

    (Klemens, 2015) Add C 101 code

--- is-Punk-Rock/an-expansive-definition-of-truth.c
+++ is-Punk-Rock/an-expansive-definition-of-truth.c
@@ -18,14 +18,10 @@ int main(){
     printf("%d \t\t\t\t",   x!=y);              printf("  x!=y              \n");
     printf("%d \t\t\t\t",   x<y);               printf("  x<y               \n");
     printf("%d \t\t\t\t",   x<=y);              printf("  x<=y              \n");
+    printf("%d \t\t\t\t",   x||y);              printf("  x||y              \n");
+    printf("%d \t\t\t\t",   x&&y);              printf("  x&&y              \n");
 
 /*
-    printf("%d \t\t\t\t",   y);                 printf("  y              \n");
-    x || y              //x or y
-
-    printf("%d \t\t\t\t",   y);                 printf("  y              \n");
-    x && y              //x and y
-
     printf("%d \t\t\t\t",   y);                 printf("  y              \n");
     x > y || y >= z     //x is greather than y or is greater than or equal to z
 
