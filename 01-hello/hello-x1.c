c890b53657668ccaa4df157428c60bc4e5dff90b
Leandro Leite <ndronos@gmail.com>
2017-02-24 11:41:24 -0300

    [Trade Tools] Fix hello app

--- 01-hello/hello-x1.c
+++ 01-hello/hello-x1.c
@@ -4,9 +4,16 @@
 int main(){
 
     printf("Initial? \n");
+    char initial;
+    scanf("%c", &initial);
+    printf("Hello, %c \n", initial);
+
+/*
+    printf("Name? \n");
     char name;
     scanf("%c", &name);
     printf("Hello, %c \n", name);
+*/
 
 }
 
