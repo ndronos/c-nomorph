e0f1038a3b0d35e620508749e99e9ae1b4eddfb1
Leandro Leite <ndronos@gmail.com>
2017-02-25 20:00:57 -0300

    (Igneus, 2015) Arrays Multidimentional code tryouts

---
+++ 01-lessons/21-array-multidimentional.c
@@ -0,0 +1,17 @@
+//  (Igneus, 2015) Advance Part in Array
+//
+//  Array Multidimentionala [rows][elements]
+//
+
+#include <stdio.h>
+
+int main() {
+
+    int salary[3][4] = {
+        {2, 3, 5, 8},
+        {8, 5, 6, 9},
+        {1, 5, 8, 6}
+    };
+
+ }
+
