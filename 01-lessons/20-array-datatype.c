4adfc67f08f0542154d0845f2818e28f1f0e3384
Leandro Leite <ndronos@gmail.com>
2017-02-25 19:46:05 -0300

    (Igneus, 2015) Arrays code tryouts

---
+++ 01-lessons/20-array-datatype.c
@@ -0,0 +1,20 @@
+//  (Igneus, 2015) Array datatype
+
+//  Arrays starts     with 0 index
+//
+//  Ways to declare
+//  1)                  double salary[9];
+//  2)                  double salary[4] = {100, 200, 500, 600}
+//  3)                  double salary[] =  {100, 200, 500, 600, 800, 900}
+
+#include <stdio.h>
+
+int main() {
+
+
+
+
+    return 0;
+
+}
+
