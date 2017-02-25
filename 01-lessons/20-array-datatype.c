fba24b2e80e9250143cc7936ab8b4681f63862c4
Leandro Leite <ndronos@gmail.com>
2017-02-25 19:47:20 -0300

    (Igneus, 2015) Arrays code tryouts

--- 01-lessons/20-array-datatype.c
+++ 01-lessons/20-array-datatype.c
@@ -6,10 +6,12 @@
 //  1)                  double salary[9];
 //  2)                  double salary[4] = {100, 200, 500, 600}
 //  3)                  double salary[] =  {100, 200, 500, 600, 800, 900}
+//  Insert value        salary[0] = 20.0;
 
 #include <stdio.h>
 
 int main() {
+
 
 
 
