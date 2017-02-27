2a7dd801fb672c8622f486ec0767852c5998e437
Leandro Leite <ndronos@gmail.com>
2017-02-26 21:37:54 -0300

    (Igneus, 2015) Strings Details code tryouts

--- 01-lessons/22-string-details.c
+++ 01-lessons/22-string-details.c
@@ -1,11 +1,22 @@
 //  (Igneus, 2015)  Details About Strings
 
+//  C is very sensitive about declarations
+
+//  `string` is an array of characters
+//            char this[10] = "THIS" ... "%s",this
+
 #include <stdio.h>
 
 int main() {
 
-    char player1 = 'w';
+    char player1 = 'w';       // = "w"; //error
     printf("%c \n", player1);
+
+    char player2[10] = "Hello";   //  player2 = "Hello"; //error    //"Hello\0"
+    printf("%s \n", player2);
+
+    char player3[10] = {'H', 'E', 'L', 'L', 'O', '\0'};   // Old way, before C99
+    printf("%s \n", player3);
 
     return 0;
 
