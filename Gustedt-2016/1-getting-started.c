df39a6c81749b3c2b00dd8aeff417e9e76b5f198
Leandro Leite <ndronos@gmail.com>
2017-02-27 17:25:39 -0300

    (Gustedt, 2016) Add Level 0. Encounter code

---
+++ Gustedt-2016/1-getting-started.c
@@ -0,0 +1,24 @@
+// (Gustedt, 2016) 0. Encounter pp. 2 Listing 1. /* This may look like nonsense, but really is -*- mode: C -*- */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* The main thing that this program does. */
+int main(void) {
+  // Declarations
+  double A[5] = {
+    [0] = 9.0,
+    [1] = 2.9,
+    [4] = 3.E+25,
+    [3] = .00007,
+  };
+
+  // Doing some work
+  for (size_t i = 0; i < 5; ++i) {
+    printf("element %zu is %g, \tits square is %g\n",
+          i,
+          A[i],
+          A[i]*A[i]);
+  }
+
+  return EXIT_SUCCESS;
+};
