dd2916efa7864cd3c084f41c816d56d6e8c19045
Leandro Leite <ndronos@gmail.com>
2017-02-28 09:57:56 -0300

    (Igneus, 2015) Add Pointers code tryouts

---
+++ Igneus-2015/23-pointers-as-simple-as-possible.c
@@ -0,0 +1,15 @@
+// (Igneus, 2015) Pointers as simple as possible
+
+#include <stdio.h>
+
+int main() {
+
+    int player1 = 4;
+    int player2[10];
+
+    printf("%x \n", player1);
+    printf("%x \n", player1);
+
+
+}
+
