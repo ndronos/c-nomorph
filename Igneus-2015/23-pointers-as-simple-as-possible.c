7c24ea3fa4547778fcd7c1ef69b9c1470b0eeb75
Leandro Leite <ndronos@gmail.com>
2017-02-28 10:05:38 -0300

    (Igneus, 2015) Add Pointers code tryouts

--- Igneus-2015/23-pointers-as-simple-as-possible.c
+++ Igneus-2015/23-pointers-as-simple-as-possible.c
@@ -1,5 +1,12 @@
 // (Igneus, 2015) Pointers as simple as possible
 
+// pointers are really the lightweight, don't take it too hard
+//          are simply a datatype that stores the memory
+// as defined, ultimatelly are grabing some memory space
+//          and placing some values in it, and later accessing those values
+//          if you want/have to access that memory location, you can use the pointer
+// like a postcard that prints the address of a person
+
 #include <stdio.h>
 
 int main() {
