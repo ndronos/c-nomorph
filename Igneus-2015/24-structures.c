69615cc9c5bfd3d784c6649c241c8710c5d644db
Leandro Leite <ndronos@gmail.com>
2017-02-28 20:24:29 -0300

    (Igneus, 2015) Add Structures code tryouts

--- Igneus-2015/24-structures.c
+++ Igneus-2015/24-structures.c
@@ -1,6 +1,8 @@
 // (Igneus, 2015) Structures
 
+// Structure: advanced array; advanced (multiple datatypes/many type of values) array (one type values)
 // Simplest possible things in C
+// Future: main reason gives such hability (inspired C++ Python)
 
 #include <stdio.h>
 #include <string.h>
