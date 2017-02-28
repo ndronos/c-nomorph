789566a806df5460a208fe75cc4227684db81c36
Leandro Leite <ndronos@gmail.com>
2017-02-28 18:58:21 -0300

    (Igneus, 2015) Add Pointers code tryouts

--- Igneus-2015/23-pointers-as-simple-as-possible.c
+++ Igneus-2015/23-pointers-as-simple-as-possible.c
@@ -6,6 +6,7 @@
 //          and placing some values in it, and later accessing those values
 //          if you want/have to access that memory location, you can use the pointer
 // like a postcard that prints the address of a person
+// declared or not, it uses memory space
 
 #include <stdio.h>
 
@@ -13,9 +14,11 @@ int main() {
 
     int player1 = 4;
     int player2[10];
+    int player3[100];
 
     printf("%x \n",  player1);  // 4
     printf("%x \n", &player2);  //player2); // eb086xxx // warning:... format unsigned int ... argument int
+    printf("%x \n", &player3); 
    // %x for memory space
 
 }
