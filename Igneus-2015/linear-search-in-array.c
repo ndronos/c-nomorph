b7d062e966238fdc3dcf09dd386ba500a288485f
Leandro Leite <ndronos@gmail.com>
2017-02-28 23:36:03 -0300

    (Igneus, 2015) Add Linear Search in Array code tryouts

---
+++ Igneus-2015/linear-search-in-array.c
@@ -0,0 +1,34 @@
+// (Igneus, 2015) Linear Search in Array
+
+#include <stdio.h>
+
+int main() {
+
+    int array[20], find, i, size, occurance = 0;
+
+    printf("Enter the size of array: ");
+    scanf("%d", &size);
+
+    printf("Enter the elements: \n");
+    for ( i = 0; i < size; i++ ) {
+        scanf("%d", &array[i]);
+    }
+
+    printf("Enter the element to search for: ");
+    scanf("%d", &find);
+
+    for ( i = 0; i < size; i++ ) {
+        if ( array[i] == find ) {
+          printf("%d is at location %d \n", find, i+1);
+          occurance++;
+        }
+    }
+
+    if (occurance == 0) {
+        printf("Value not in the array!");
+    } else {
+        printf("Your element occurred %d times", occurance);
+    } 
+
+}
+
