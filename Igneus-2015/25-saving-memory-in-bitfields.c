d08febacdecce2ae556d9bd68b0586e05581b966
Leandro Leite <ndronos@gmail.com>
2017-02-28 21:11:39 -0300

    (Igneus, 2015) Add Memory Bitfields code tryouts

--- Igneus-2015/25-saving-memory-in-bitfields.c
+++ Igneus-2015/25-saving-memory-in-bitfields.c
@@ -1,6 +1,7 @@
 // (Ingneus, 2015) Saving Memory in Bitfields
 
 // in C you have to care anything and everything
+// in return, C allows you to control the memory 
 
 #include <stdio.h>
 
@@ -9,10 +10,19 @@ struct {
     unsigned int highMemory;
 } mem1;
 
+struct {
+    unsigned int lowMemory  : 1;
+    unsigned int highMemory : 1;
+} mem2;
+
 int main() {
 
     printf("The memory size occupied by mem1 is: %lu bytes \n", sizeof(mem1));
 
-    return 0;
+    printf("The memory size occupied by mem2 is: %lu bytes \n", sizeof(mem2));
+
 }
+
+//  unsigned int highMemory;  // unsigned int is 4 bytes or 32 bits
+//  unsigned int highMemory : 1        // out of 4 bytes or 32 bits, only use 1 bit
 
