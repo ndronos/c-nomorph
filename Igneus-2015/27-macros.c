22bccdac7f7fc9532cb9a85c0acb500b9f3295d3
Leandro Leite <ndronos@gmail.com>
2017-02-28 21:33:42 -0300

    (Igneus, 2015) Add Macros code tryouts

--- Igneus-2015/27-macros.c
+++ Igneus-2015/27-macros.c
@@ -5,7 +5,10 @@
 int main() {
 
     printf("%s \n", __FILE__);
-
+    printf("%s \n", __DATE__);
+    printf("%s \n", __TIME__);
+    printf("%d \n", __LINE__);
+    printf("%d \n", __STDC__);
 
 }
 
