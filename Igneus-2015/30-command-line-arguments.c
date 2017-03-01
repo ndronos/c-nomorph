891089b4383269fcbc2e7177a164a6e705d9e89b
Leandro Leite <ndronos@gmail.com>
2017-02-28 22:43:05 -0300

    (Igneus, 2015) Add Command Line Arguments code tryouts

--- Igneus-2015/30-command-line-arguments.c
+++ Igneus-2015/30-command-line-arguments.c
@@ -1,12 +1,18 @@
 // (Igneus, 2015) Command Line Arguments
 
+//  argv[]    pass arguments and additional information, an array of arguments
+//  argv[0]   Application Name        // 0 element is the 1st element
+//  argv[1]   Argument                // argc == 2
+//  argv[2]
+//  argv[...
+
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
 
     printf("Program name %s \n", argv[0]);
 
-    if ( argc ==2 )
+    if ( argc == 2 )
     {
         printf("The argument supplied is %s \n", argv[1]);
     }
