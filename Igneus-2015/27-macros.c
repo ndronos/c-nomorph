012ea0911d94422894b30620bd9fa60e8ff65c99
Leandro Leite <ndronos@gmail.com>
2017-02-28 21:38:50 -0300

    (Igneus, 2015) Add Macros code tryouts

--- Igneus-2015/27-macros.c
+++ Igneus-2015/27-macros.c
@@ -1,5 +1,9 @@
 // (Igneus, 2015) Macros in C
 
+// Predefine sort of funtions
+// Not a lot, just 6 of them, every macros starts/ends double undescores: __
+// Uses: adm server, file handling
+
 #include <stdio.h>
 
 int main() {
@@ -8,7 +12,7 @@ int main() {
     printf("%s \n", __DATE__);
     printf("%s \n", __TIME__);
     printf("%d \n", __LINE__);
-    printf("%d \n", __STDC__);
+    printf("%d \n", __STDC__);  //Always return 1, if compiled with C standards
 
 }
 
