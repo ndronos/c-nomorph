ef9d56a5d51d9bee1fe488d65d5548522bdf18a5
Leandro Leite <ndronos@gmail.com>
2017-03-03 12:30:24 -0300

    (HowStuffWorks, 2017) Add 9 Looping A Real Example code

--- HowStufWorks-2017/9-looping-f2c-while-bugix.c
+++ HowStufWorks-2017/9-looping-f2c-while-bugix.c
@@ -7,14 +7,25 @@
 
 int main() {
 
-    int a;
+    int a, b;
     a =0;
+    
+    b = -1;           // b as in bullshit... 
+    double h = 98.6;  // human normal temperature
 
-    while (a <= 200) {    // 100) {
+    while (a <= 100) {    // 200) {
 
-        printf("%6.2f degrees F = %6.2f degrees C \n", a*1.0, (a - 32.0) * 5.0 / 9.0 ); // `a` solved 
-    //  printf("%6.2f degrees F = %6.2f degrees C \n", a,     (a - 32.0) * 5.0 / 9.0 ); // `a` problem 
-    //  printf("%4d   degrees F = %4d   degrees C \n", a, (a - 32  ) * 5/9   );
+        if ( (a > h) && (b < h) ) {
+
+          printf("%6.2f degrees F = %6.2f degrees C \n", h*1.0, (h - 32.0) * 5.0 / 9.0 ); // `a` solved 
+
+        } else {
+
+          printf("%6.2f degrees F = %6.2f degrees C \n", a*1.0, (a - 32.0) * 5.0 / 9.0 ); // `a` solved 
+      //  printf("%6.2f degrees F = %6.2f degrees C \n", a,     (a - 32.0) * 5.0 / 9.0 ); // `a` problem 
+      //  printf("%4d   degrees F = %4d   degrees C \n", a, (a - 32  ) * 5/9   );
+
+        }
         a = a + 10;
 
     }
