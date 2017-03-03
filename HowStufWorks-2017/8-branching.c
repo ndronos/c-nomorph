257fd56d37c9487f41acfd9930697f822c31563b
Leandro Leite <ndronos@gmail.com>
2017-03-02 23:43:25 -0300

    (HowStufWorks, 2017) Add 8 Branching and Looping code

---
+++ HowStufWorks-2017/8-branching.c
@@ -0,0 +1,17 @@
+// (HowStuffWorks) Branching and Looping pp. 8
+
+#include <stdio.h>
+
+int main() {
+
+    int b;
+    printf("Enter a value: ");
+    scanf("%d", &b);
+
+    if (b < 0)
+        printf("The value is negative. \n");
+
+    return 0;
+
+}
+
