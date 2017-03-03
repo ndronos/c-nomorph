ba09c845dec6c6fdd33caf2218db84c7be24ce93
Leandro Leite <ndronos@gmail.com>
2017-03-03 00:38:53 -0300

    (HowStufWorks, 2017) Add 8 Branching and Looping code

---
+++ HowStufWorks-2017/8-if-true.c
@@ -0,0 +1,18 @@
+// (HowStuffWorks) Branching and Looping pp. 8
+
+#include <stdio.h>
+
+int main() {
+
+    int a;
+
+    printf("Enter a value: ");
+    scanf("%d", &a);
+
+    if (a)
+        printf("The value is True. \n");
+
+    return 0;
+
+}
+
