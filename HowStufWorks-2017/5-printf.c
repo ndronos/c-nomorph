094cbb79a1edeb5df065ed70984f30f74656e900
Leandro Leite <ndronos@gmail.com>
2017-03-02 22:15:15 -0300

    (HowStuffWorks, 2017) Add 5 Printf code

---
+++ HowStufWorks-2017/5-printf.c
@@ -0,0 +1,15 @@
+// (HowStuffWorks, 2017) Printf pp. 5
+
+#include <stdio.h>
+
+int main() {
+
+    int a, b, c;
+    a = 5;
+    b = 7;
+    c = a + b;
+    printf("%d + %d = %d \n", a, b, c);
+    return 0;
+
+}
+    
