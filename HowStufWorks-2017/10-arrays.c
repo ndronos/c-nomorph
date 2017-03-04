75811533b7cbf8f9c63b40b131689bbf2c2bb8ab
Leandro Leite <ndronos@gmail.com>
2017-03-04 00:38:42 -0300

    (HowStuffWorks, 2017) Add 10 Arrays notes

--- HowStufWorks-2017/10-arrays.c
+++ HowStufWorks-2017/10-arrays.c
@@ -1,6 +1,13 @@
-// (HowStuffWorks, 2017) Arrays pp. 10
+// (HowStuffWorks, 2017) Arrays pp. 10-11
 
 #include <stdio.h>
+
+// Note:
+int   a[10];        // array of integers
+char  s[10];        // array of characters, a C string
+float f[20];        // array of reals
+struct rec r[50];   // array of records
+
 
 int main() {
 
