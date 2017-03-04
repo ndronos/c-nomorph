6e74f584ef808891f7c1b59794f14be4c955bb7d
Leandro Leite <ndronos@gmail.com>
2017-03-04 00:34:25 -0300

    (HowStuffWorks, 2017) Add 11 Structures code

--- HowStufWorks-2017/11-structures.c
+++ HowStufWorks-2017/11-structures.c
@@ -1,26 +1,29 @@
 // (HowStuffWorks, 2017) Structures pp. 11
 
 #include <stdio.h>
-/*
-struct rec {       // structure of the type rec
+
+struct One {       // structure of the type `One`
     int a, b, c;
     float d, e, f;
 };
-struct rec r;
-*/
-struct rec {       // compressed form
+struct One o;
+
+struct Two {       // compressed form
     int a, b, c;
     float d, e, f;
-} r;               // type declaration for rec and variable r on same statement
+} t;               // type declaration for `Two` and variable `t` on same statement
 
 /*
 typedef struct rec rec_type;  // create a typedef statement for the structure name
 rec_type r;                  // then declare records of type `rec_type`
 */
-r.a = 5;            // access fields of a structure using a period
+
 
 int main() {
+    o.a = 9;
+    t.a = 5;            // access fields of a structure using a period
 
-    printf("%d \n", r.a);
+    printf("%d \n", o.a);
+    printf("%d \n", t.a);
 
 }
