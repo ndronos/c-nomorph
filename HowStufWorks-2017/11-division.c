2909e099d0d5f501d3cb9104a5efb003a2257eab
Leandro Leite <ndronos@gmail.com>
2017-03-03 21:32:26 -0300

    (HowStuffWorks, 2017) Add 11 More on Arrays code

---
+++ HowStufWorks-2017/11-division.c
@@ -0,0 +1,13 @@
+// (HowStuffWorks, 2017) More on Arrays Variable Types pp. 11
+
+#include <stdio.h>
+
+int main() {
+
+    float a;
+    a = 10 / 3;         // 3.000000
+    printf("%f \n", a);
+
+
+}
+
