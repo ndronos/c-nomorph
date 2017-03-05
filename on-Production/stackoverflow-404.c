31a6c88916499430987219dd501df29b77f88830
Leandro Leite <ndronos@gmail.com>
2017-03-05 00:28:22 -0300

    (StackOverflow) 404 code attempt

---
+++ on-Production/stackoverflow-404.c
@@ -0,0 +1,14 @@
+// StackOverflow
+
+
+  # define v putchar
+  #   define print(x)
+  main(){v(4+v(v(52)-4));return 0;}/*#>++++++++4+[>++++++<-]>
+  ++++.----.++++.*/
+  print(202*2);exit();
+  #define/*>.@*/exit()
+
+/*
+#include <stdio.h>
+int main(){ printf("%d\n",202*2); }
+*/
