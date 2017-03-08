34eb66d7fe57db306d5847ce8b8840e67a31d1d0
Leandro Leite <ndronos@gmail.com>
2017-03-07 21:33:28 -0300

    (HowStuffWorks, 2017) Add Functions code

--- HowStufWorks-2017/12-function-parameters.c
+++ HowStufWorks-2017/12-function-parameters.c
@@ -2,11 +2,25 @@
 
 #include <stdio.h>
 
+int factorial(int i) {
+
+    int j, k;
+
+    j = 1;
+
+    for (k = 2; k <= i; k++)
+        j = j * k;
+
+    return j;
+
+}
 
 
 int main() {
 
+    int x = 4;
 
+    printf("Factorial of %d is %d. \n", x, factorial(4));
 
 }
 
