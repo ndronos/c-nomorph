acb4e3df3be63c11844ca341a05c3208689e0ae4
Leandro Leite <ndronos@gmail.com>
2017-03-22 19:57:39 -0300

    (Tutorialspoint, 2017) Add Basic Syntax code

---
+++ TutorialsPoint-2017/4-hello-tokens.c
@@ -0,0 +1,16 @@
+// (Tutorialspoint, 2017) Basic Syntax
+
+#include <stdio.h>
+
+int main() {
+
+  printf("Hello, World! \n");     // Statement consists of five individual tokens:
+
+  printf
+  (
+  "Hello, World! \n"
+  )
+  ;
+
+}
+
